3d11468da05e7689f80c8eb8cb7f8f2210654179
Alecaddd <castellani.ale@gmail.com>
2018-04-08 22:33:20 -0700

    Updated composer dependencies, improved safety checks, initial round of questions for further customization

--- src/NewCommand.php
+++ src/NewCommand.php
@@ -11,14 +11,15 @@
 
 namespace Awps;
 
+use ZipArchive;
+use RuntimeException;
+use GuzzleHttp\ClientInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use GuzzleHttp\ClientInterface;
 use Symfony\Component\Console\Question\Question;
-use ZipArchive;
 
 /**
  * Generate a new installation of AWPS
@@ -47,9 +48,12 @@ class NewCommand extends Command
 	 */
 	public function configure()
 	{
-		$this->setName('new')
-				->setDescription('Create a new AWPS Theme installation')
-				->addArgument('name', InputArgument::REQUIRED, 'Insert the folder name');
+		$this
+			->setName('new')
+			->setDescription('Create a new AWPS Theme installation')
+			->addArgument('name', InputArgument::REQUIRED, 'Insert the folder name')
+			->addOption('dev', null, InputOption::VALUE_NONE, 'Download the latest "development" release')
+			->addOption('force', null, InputOption::VALUE_NONE, 'Forces the install even if the directory already exists');
 	}
 
 	/**
@@ -61,25 +65,37 @@ class NewCommand extends Command
 	 */
 	public function execute(InputInterface $input, OutputInterface $output)
 	{
-		$themeName = $input->getArgument('name');
-		$directory = getcwd().'/'.$themeName;
+		if (! class_exists('ZipArchive')) {
+			throw new RuntimeException('The Zip PHP extension is not installed. Please install it and try again.');
+		}
+
+		$folderName = $input->getArgument('name');
+		$directory = getcwd().'/'.$folderName;
 
 		$output->writeln('<info>Summoning application..</info>');
 
-		$this->assertApplicationDoesNotExists($directory, $output);
+		if (! $input->getOption('force')) {
+			$this->assertApplicationDoesNotExists($directory, $output);
+		}
 
 		$this->assertLocationInsideWordPress($directory, $output);
 
 		$helper = $this->getHelper("question");
 
+		$question = new Question("Name of your theme? <info>(Awps)</info> ", "Awps");
+		$themeName = $helper->ask($input, $output, $question);
+
 		$question = new Question("PHP Namespace of your theme? <info>(awps)</info> ", "awps");
-		$namespace = $helper->ask($input, $output, strtolower($question));
+		$namespace = $helper->ask($input, $output, $question);
+
+		$question = new Question("Description? <info>(Alecaddd WordPress Starter theme)</info> ", "Alecaddd WordPress Starter theme");
+		$description = $helper->ask($input, $output, $question);
 
 		$output->writeln('<info>Downloading Package..</info>');
 
 		$this->download($ZipFile = $this->makeFileName())
 				->extract($ZipFile, $directory, $output)
-				->renameNamespaces($directory, $output, $themeName, $namespace)
+				->renameNamespaces($directory, $output, $folderName, $namespace)
 				->cleanUp($ZipFile);
 
 		$output->writeln('<info>Updating config files..</info>');
@@ -114,7 +130,7 @@ class NewCommand extends Command
 	 */
 	private function assertLocationInsideWordPress($directory, OutputInterface $output)
 	{
-		if (!strpos(dirname($directory), 'wp-content')) {
+		if (! strpos(dirname($directory), 'wp-content')) {
 			$output->writeln('<error>This is not a WordPress theme directory!</error>');
 			exit(1);
 		}
@@ -163,7 +179,7 @@ class NewCommand extends Command
 
 		$archive->close();
 
-		if (!empty(shell_exec("mv ".$directory."/awps-master/* ".$directory."/awps-master/.[!.]*  ".$directory))){
+		if (! empty(shell_exec("mv ".$directory."/awps-master/* ".$directory."/awps-master/.[!.]*  ".$directory))) {
 			$output->writeln('<comment>Unable to move files from master folder!</comment>');
 
 			return $this;
@@ -179,13 +195,13 @@ class NewCommand extends Command
 	 *
 	 * @param string $directory
 	 * @param OutputInterface $output
-	 * @param string $themeName
+	 * @param string $folderName
 	 * @param string $namespace
 	 * @return void
 	 */
-	private function renameNamespaces($directory, OutputInterface $output, $themeName = null, $namespace = null)
+	private function renameNamespaces($directory, OutputInterface $output, $folderName = null, $namespace = null)
 	{
-		if (is_null($themeName) && is_null($namespace)) {
+		if (is_null($folderName) && is_null($namespace)) {
 			return $this;
 		}
 
@@ -196,13 +212,13 @@ class NewCommand extends Command
 		foreach ($file_info as $file) {
 			$str = file_get_contents($file);
 
-			if (!is_null($namespace)) {
+			if (! is_null($namespace)) {
 				$str = str_replace("Awps", $namespace, $str);
 				$str = str_replace("awps", $namespace, $str);
 			}
 			
-			if (!is_null($themeName)) {
-				$str = str_replace("Alecaddd WordPress Starter theme", $themeName, $str);
+			if (! is_null($folderName)) {
+				$str = str_replace("Alecaddd WordPress Starter theme", $folderName, $str);
 			}
 
 			file_put_contents($file, $str);
@@ -214,7 +230,7 @@ class NewCommand extends Command
 	private function recursiveScanFiles($path, &$file_info)
 	{
 		$path = rtrim($path, '/');
-		if (!is_dir($path)) {
+		if (! is_dir($path)) {
 			$file_info[] = $path;
 		} else {
 			$files = scandir($path);
@@ -236,11 +252,18 @@ class NewCommand extends Command
 
 	private function moveConfig($directory, OutputInterface $output)
 	{
-		if (!empty(shell_exec("mv ".$directory."/wp-config.sample.php ../../"))) {
+		if (! empty(shell_exec("mv ".$directory."/wp-config.sample.php ../../"))) {
 			$output->writeln('<comment>Unable to move the wp-config.sample.php file, be sure to move it in the base directory!</comment>');
 		}
 
-		if (!empty(shell_exec("mv ".$directory."/.env.example ../../.env"))) {
+		if (is_file("../../.env")) {
+			if (! empty(shell_exec("mv ../../.env ../../.env.bk"))) {
+				$output->writeln('<comment>Existing .env detected in the WordPress root directory, unable to create a safe backup!</comment>');
+				return $this;
+			}
+		}
+
+		if (! empty(shell_exec("mv ".$directory."/.env.example ../../.env"))) {
 			$output->writeln('<comment>Unable to move the .env.example file, be sure to move it in the base directory and rename it as .env!</comment>');
 		}
 
